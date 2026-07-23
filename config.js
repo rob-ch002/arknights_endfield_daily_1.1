2d6963c359f38b4ca54f8a867ddf97edd7706e82
rob-ch002 <121871435+rob-ch002@users.noreply.github.com>
2026-07-24 05:44:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ window.ENDFIELD_CONFIG = Object.freeze({
   performanceModeDefault: "balanced",
 
   /* Isi dengan public VAPID key dari GitHub secret VAPID_PUBLIC_KEY. */
-  pushVapidPublicKey: "BHIEtGmiEJk9vocX2h0mWYzZr7t34_SQK50j5jWk5RxdMj78FFRrBWp0KuDwSsUtceMer-efK8TuPVEioqFVfBE",
+  pushVapidPublicKey: "BGtkbcjrO12YMoDuq2sCQeHlu47uPx3SHTgFKZFYiBW8Qr0D9vgyZSZPdw6_4ZFEI9Snk1VEAj2qTYI1I1YxBXE",
 
   // PIN default: 123456
   pinSha256:
